4f2269a84d04d2bac5c45d5c888b6d376ce0fd78
Ioannis Michaloliakos <ioannis.michalol@ufl.edu>
2024-12-03 07:50:13 +0000

    clean solution day3b

--- day03/puzzle2.jl
+++ day03/puzzle2.jl
@@ -1,16 +1,11 @@
 function sum_muls()
-    # remove all segments that are of the form "don't().*do()"
     remove_pattern = r"don't\(\).*?(?:do\(\)|$)"
     mul_pattern = r"mul\((\d+),(\d+)\)"
     total = 0
     open("./inputs/input.txt", "r") do file
-        # merge all lines into a single line
         full_text = join(readlines(file), "")
-        # println(line)
         full_text = replace(full_text, remove_pattern => "-")
-        # println(line)
         all_matches = eachmatch(mul_pattern, full_text)
-        # println(all_matches)
         for match in all_matches
             num1 = parse(Int, match.captures[1])
             num2 = parse(Int, match.captures[2])
